af2ff9388d18221b909145060fc11f4933ef1d5a
suryaajis <suryaajis330@gmail.com>
2026-07-09 12:16:41 +0000

    feat(api): register DebtsModule and Debt entity in AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -8,6 +8,7 @@ import { TransactionsModule } from './transactions/transactions.module';
 import { BudgetsModule } from './budgets/budgets.module';
 import { BackupModule } from './backup/backup.module';
 import { RecurringModule } from './recurring/recurring.module';
+import { DebtsModule } from './debts/debts.module';
 import { buildDataSourceOptions } from './database/data-source-options';
 
 @Module({
@@ -26,6 +27,7 @@ import { buildDataSourceOptions } from './database/data-source-options';
     BudgetsModule,
     BackupModule,
     RecurringModule,
+    DebtsModule,
   ],
 })
 export class AppModule {}
